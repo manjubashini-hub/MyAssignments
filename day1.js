8786ab5e5b13a835b7c434ff0382d9e8fd63307d
manjubashini-hub <manjubashinie@gmail.com>
2026-04-26 07:09:04 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,28 @@
+
+var firstName ="Manju"
+console.log(firstName)
+console.log(typeof(firstName))
+
+var companyName="XXX"
+console.log(companyName)
+console.log(typeof(companyName))
+
+var phoneNumber=8270002579
+console.log(phoneNumber)
+console.log(typeof(phoneNumber))
+
+var isAutomation=true
+console.log(isAutomation)
+console.log(typeof(isAutomation))
+
+var hasPlayWright
+console.log(hasPlayWright)
+console.log(typeof(hasPlayWright))
+
+var codeDone=null
+console.log(codeDone)
+console.log(typeof(codeDone))
+
+
+
+
